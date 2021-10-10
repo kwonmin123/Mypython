6eb3bb75ac660cc1516c50a95ebb2be336a0b7c1
kwonmin123123 <zzacjk@naver.com>
2021-10-10 22:04:29 +0900

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,9 @@
+from random import *
+
+print(random())#0~1미만의 수
+print(random()*10)#0~10미만의 모든 수
+print(int(random()*10)) #0~10미만의 정수값
+print(int(random()*10)+1) #1~10이하의 정수값
+print(int(random()*45)+1) #1~45이하의 정수값
+print(randrange(1,46))#1~46미만의 정수값
+print(randint(1,45))#1~45이하의 정수값
