e31b828176f08bc13a066dd45b7634c3686acff2
kwonmin123123 <zzacjk@naver.com>
2021-10-10 21:21:42 +0900

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,13 @@
+String ="hi"
+String2 ='난 천재'# ' or " 사용가능
+age =14 #숫자 그냥적음
+boolean = 10>3 #true
+boolean2 = 10<3#false
+
+print(String,String2,age,boolean)#쉼표는 타입변화에 유연함
+print(String+String2+str(age)+str(boolean2))#+는 타입변화에 경직됨
+print(f"이게 머냐면 {age} 변수를 {String} 가져다{boolean} 쓰는 또다른 방법")
+
+'''이렇게 하면
+여러문장이
+주석이 됨'''
