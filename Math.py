69c87bd6cafabfa09b871533b17412206c4c71a6
kwonmin123123 <zzacjk@naver.com>
2021-10-10 22:04:22 +0900

    Create Math.py

---
+++ Math.py
@@ -0,0 +1,20 @@
+print(1) #1
+print(3*4)# 
+print(3**4)# 
+print(17/6)#자바처럼 몫이 나오지 않음
+print(17%6)#나머지
+# 그렇다면 몫을 구하고싶으면?
+print("몫을구하고싶으면 //를 써라 " + str(17//6))
+# print("몫을구하고싶으면 //를 써라")
+print(abs(-30))
+print(pow(5,2))
+print(max(12,17))
+print(min(30,10))
+print(round(3.14))
+print(round(7.74))
+
+from math import *
+print(floor(4.99))
+print(ceil(4.99))
+print(sqrt(25))
+print(sqrt(17))
