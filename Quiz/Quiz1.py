05e4a470fc735b3a7e265c766c71ab8e5143d126
kwonmin123123 <zzacjk@naver.com>
2021-10-10 21:26:22 +0900

    Create Quiz1.py

---
+++ Quiz/Quiz1.py
@@ -0,0 +1,17 @@
+'''변수명 : station
+    변수값 : 사당역, 서울역, 강남역 
+
+출력값 : 변수값 행 열차가 들어오고있습니다
+을 출력하는 코드 작성
+'''
+
+station = ""
+
+station = "사당역"
+print(f'{station} 행 열차가 들어오고있습니다')
+
+station = "서울역"
+print(f'{station} 행 열차가 들어오고있습니다')
+
+station = "강남역"
+print(f'{station} 행 열차가 들어오고있습니다')
