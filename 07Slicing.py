a5cab6ca62e08e6b55f6a86f409dd40598d66710
kwonmin123123 <zzacjk@naver.com>
2021-10-10 22:32:21 +0900

    Create 07Slicing.py

---
+++ 07Slicing.py
@@ -0,0 +1,6 @@
+IDnum = '990107-1234567'
+print("첫번째 숫자 IDnum[0] :"+ IDnum[0])
+print("첫 두 숫자 IDnum[:2] "+ IDnum[:2])
+print(f"월을 표시하는 숫자 IDnum[2:4] : {IDnum[2:4]}")
+print("마지막 숫자 IDnum[-1] :",IDnum[-1])
+print("마지막 7자리 숫자 IDnum[-7:] :",IDnum[-7:])
