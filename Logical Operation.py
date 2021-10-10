ff08362c7e19cd68911c8c1b027a1af42fcae981
kwonmin123123 <zzacjk@naver.com>
2021-10-10 21:43:33 +0900

    Create Logical Operation.py

---
+++ Logical Operation.py
@@ -0,0 +1,18 @@
+#or a b중 둘중하나면 됨
+a= True
+b= False
+c= True
+d= False
+print(a or b)
+print(a | c)
+#and 둘다 참이여야 참
+print(a and b)
+print(a & c)
+
+#nor  둘중하나만 참이여아함
+print(a ^ b)
+print(a ^ c)
+
+# == 같음
+print(a==b)
+print(a==c)
